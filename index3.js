33301e761a76ccc3014b9b76eecc4462c38e9912
Dani Vicario <danigv.email@gmail.com>
2019-03-21 11:55:46 +0100

    added mixed map, filter, reduce example

--- index3.js
+++ index3.js
@@ -1,31 +1,31 @@
 var names = ["pepe", "ernesto", "edu"]
 
 let output = names.map(name => {
-    return {name: name, id: Math.floor(Math.random() * 10000)}
+    return { name: name, id: Math.floor(Math.random() * 10000) }
 })
 
 // console.log(names)
 // console.log(output)
 
 let employees = [
-    {name: "dani", dept: "teacher"},
-    {name: "marina", dept: "admissions"},
-    {name: "victor", dept: "pm"}
+    { name: "dani", dept: "teacher" },
+    { name: "marina", dept: "admissions" },
+    { name: "victor", dept: "pm" }
 ]
 
 let employeesWithSalaries = employees.map(employee => {
     let salary;
-    
+
     if (employee.dept === "teacher") {
-        salary = 100000    
+        salary = 100000
     }
 
     if (employee.dept === "admissions") {
-        salary = 200000    
+        salary = 200000
     }
 
     if (employee.dept === "pm") {
-        salary = 300000    
+        salary = 300000
     }
 
     return {
@@ -36,8 +36,21 @@ let employeesWithSalaries = employees.map(employee => {
 
 // console.log(employeesWithSalaries)
 
-var filteredHighSalaries = 
-employeesWithSalaries.filter(employee => employee.salary > 100000)
+function calculateHighedPaidEmployees(highSalaryCriteria) {
+    if (highSalaryCriteria === undefined)   return;
 
-console.log(filteredHighSalaries)
-console.log(filteredHighSalaries.length)
+    var filteredHighSalaries =
+        employeesWithSalaries
+            .filter(employee => employee.salary > highSalaryCriteria)
+    
+    var totalAmountHighSalary = 
+        filteredHighSalaries 
+            .reduce((ac, cValue) => ac + cValue.salary, 0)
+
+    console.log(filteredHighSalaries)
+    console.log(`Your highed paid employees (with a salary of ${highSalaryCriteria}) 
+    totalize ${filteredHighSalaries.length}`)
+    console.log(`You are spending ${totalAmountHighSalary} eur in high salaries`)
+}
+
+calculateHighedPaidEmployees(200000)
